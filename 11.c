3e1a9cacfaddcaab6e5848abc7d9bc0919be06c3
Caio Arruda <e.clemes@defensoria.sp.def.br>
2022-11-14 21:14:24 -0300

    Correção ex11

--- 11.c
+++ 11.c
@@ -1,40 +1,38 @@
+#include <stdio.h>
 int main()
 {
-    int dia_ini, dia_fim;
-    int tot_dias;
-    int tot_horas;
-    int tot_minutos;
-    int tot_segundos;
+    int dia, diaF, hora, horaF, minuto, minutoF, segundo, segundoF;
 
-    int hora_ini, minuto_ini, segundo_ini;
-    int hora_fim, minuto_fim, segundo_fim;
+    scanf("Dia %d", &dia);
+    scanf("%d : %d : %d\n", &hora, &minuto, &segundo);
+    scanf("Dia %d", &diaF);
+    scanf("%d : %d : %d", &horaF, &minutoF, &segundoF);
 
-    scanf("Dia %d", &dia_ini);
-    scanf("%d : %d : %d\n", &hora_ini, &minuto_ini, &segundo_ini);
-    scanf("Dia %d", &dia_fim);
-    scanf("%d : %d : %d", &hora_fim, &minuto_fim, &segundo_fim);
+    segundo = segundoF - segundo;
+    minuto = minutoF - minuto;
+    hora = horaF - hora;
+    dia = diaF - dia;
 
-    tot_dias = dia_fim - dia_ini;
-    tot_horas = hora_fim - hora_ini;
-    tot_minutos = minuto_fim - minuto_ini;
-    tot_segundos = segundo_fim - segundo_ini;
-
-    if (tot_horas < 0)
+    if (segundo < 0)
     {
-        tot_horas += 24;
-        tot_dias -= 1;
-    }
-    if (tot_minutos < 0)
-    {
-        tot_minutos += 60;
-        tot_horas -= 1;
-    }
-    if (tot_segundos < 0)
-    {
-        tot_segundos += 60;
-        tot_minutos -= 1;
+        segundo += 60;
+        minuto--;
     }
 
-    printf("%d dia(s)\n%d hora(s)\n%d minuto(s)\n%d segundo(s)\n", tot_dias, tot_horas, tot_minutos, tot_segundos);
-    return 0;
+    if (minuto < 0)
+    {
+        minuto += 60;
+        hora--;
+    }
+
+    if (hora < 0)
+    {
+        hora += 24;
+        dia--;
+    }
+
+    printf("%d dia(s)\n", dia);
+    printf("%d hora(s)\n", hora);
+    printf("%d minuto(s)\n", minuto);
+    printf("%d segundo(s)\n", segundo);
 }
