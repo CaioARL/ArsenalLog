a24c53d6275034c02a1fa7178bc0a4e80733b72e
Caio Arruda <e.clemes@defensoria.sp.def.br>
2022-11-19 17:17:33 -0300

    corrigindo formatação

--- linha_e_coluna.c
+++ linha_e_coluna.c
@@ -21,28 +21,29 @@ int main()
     while (sair[0] != 's')
     {
         printf("linha escolhida:\n");
-    scanf("%d", &linha);
-    printf("coluna escolhida:\n");
-    scanf("%d", &coluna);
-    printf("Operacao a realizar (S = soma, M = Media):\n");
-    scanf("%s", &opera);
+        scanf("%d", &linha);
+        printf("coluna escolhida:\n");
+        scanf("%d", &coluna);
+        printf("Operacao a realizar (S = soma, M = Media):\n");
+        scanf("%s", &opera);
 
-    for (j = 0; j < 12; j++)
-    {
-        somaLinha += matriz[linha - 1][j];
-        somaColuna += matriz[j][coluna - 1];
-    }
+        for (j = 0; j < 12; j++)
+        {
+            somaLinha += matriz[linha - 1][j];
+            somaColuna += matriz[j][coluna - 1];
+        }
 
-    if (opera[0] == 'S')
-    {
-        printf("Soma da linha: %.2f\nSoma da coluna: %.2f\n", somaLinha, somaColuna);
-    }else if (opera[0] == 'M')
-    {
-        printf("Media da linha: %.2f\nMedia da coluna: %.2f\n", somaLinha/12, somaColuna/12);
-    }
+        if (opera[0] == 'S')
+        {
+            printf("Soma da linha: %.2f\nSoma da coluna: %.2f\n", somaLinha, somaColuna);
+        }
+        else if (opera[0] == 'M')
+        {
+            printf("Media da linha: %.2f\nMedia da coluna: %.2f\n", somaLinha / 12, somaColuna / 12);
+        }
 
-    printf("\n\tDeseja sair? (s = sim, n = nao)\n");
-    scanf("%s", &sair);
+        printf("\n\tDeseja sair? (s = sim, n = nao)\n");
+        scanf("%s", &sair);
     }
 
     return 0;
